31da45f551256bb9ac761253780853d446f77421
echeverriaalex <aleex.naahuel@outlook.com>
2020-10-09 00:44:01 -0300

    agrego address con set y get

--- Models/Cinema.php
+++ Models/Cinema.php
@@ -5,6 +5,7 @@
 
         private $name;
         private $capacity;
+        private $address;
 
         public function __construct($name = '', $capacity = ''){
 
@@ -17,5 +18,8 @@
 
         public function setCapacity($capacity){$this->capacity = $capacity;}
         public function getCapacity(){return $this->capacity;}
+
+        public function setAddress($address){$this->address = $address;}
+        public function getAddress(){return $this->address;}
     }
 ?>
