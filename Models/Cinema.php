dc8e84f8a37ad047f05513dcc131777a73bc35db
echeverriaalex <aleex.naahuel@outlook.com>
2020-10-09 00:35:02 -0300

    creacion clase cinema con get y set del atributo name

---
+++ Models/Cinema.php
@@ -0,0 +1,19 @@
+<?php
+    namespace Models;
+
+    class Cinema{
+
+        private $name;
+
+        public function __construct($name = ''){
+
+            $this->setName($name);
+            
+        }
+
+        public function setName($name){$this->name = $name;}
+        public function getName(){return $this->name;}
+
+    }
+
+?>
