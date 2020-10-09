3c86b4d80534cd3ebfccc118ed8303054a811b02
echeverriaalex <aleex.naahuel@outlook.com>
2020-10-09 00:40:30 -0300

    agrego el atributo capacity con su set y get

--- Models/Cinema.php
+++ Models/Cinema.php
@@ -4,16 +4,18 @@
     class Cinema{
 
         private $name;
+        private $capacity;
 
-        public function __construct($name = ''){
+        public function __construct($name = '', $capacity = ''){
 
             $this->setName($name);
-            
+            $this->setCapacity($capacity);
         }
 
         public function setName($name){$this->name = $name;}
         public function getName(){return $this->name;}
 
+        public function setCapacity($capacity){$this->capacity = $capacity;}
+        public function getCapacity(){return $this->capacity;}
     }
-
 ?>
