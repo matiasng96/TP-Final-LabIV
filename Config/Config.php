4ea5c5f3ffee5cf92deab8c69dca22d1412cb86f
echeverriaalex <aleex.naahuel@outlook.com>
2020-10-10 00:24:34 -0300

    actualizacion de constantes hasta llegar a una en comun

--- Config/Config.php
+++ Config/Config.php
@@ -2,12 +2,15 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", "/TPFinal/MoviePass/");
+
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+
+# front para Alex  
+define("FRONT_ROOT", "/TP-Final-LabIV/");
+
+# front para Mati
+//define("FRONT_ROOT", "/TPFinal/MoviePass/");
+
 ?>
-
-
-
-
