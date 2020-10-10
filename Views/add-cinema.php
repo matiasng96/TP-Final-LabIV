fea100de1609c4b0f2b1cea567bb4ce8639b2546
echeverriaalex <aleex.naahuel@outlook.com>
2020-10-09 21:09:26 -0300

    actualizacion

--- Views/add-cinema.php
+++ Views/add-cinema.php
@@ -4,47 +4,54 @@
 ?>
 
 <main>
-    <div>
-        <h1> Agregar Cinema </h1>
-    </div>
+    <form>
+        <div>
+            <h1> Agregar Cinema </h1>
+        </div>
 
-    <div>
-        <label for="name"> Nombre </label>
-        <input name="name" id="name" >
-    </div>
+        <div>
+            <label for="name"> Nombre </label>
+            <input type="text" name="name" id="name" >
+        </div>
 
-    <div>
-        <label for=""> Capacidad maxima </label>
-        <input name="">
-    </div>
+        <div>
+            <label for=""> Capacidad maxima </label>
+            <input type="number" name="">
+        </div>
 
-    <div>
-        <label for=""> Direccion </label>
-        <input name="address" id="address">
-    </div>
+        <div>
+            <label for=""> Direccion </label>
+            <input type="text" name="address" id="address">
+        </div>
 
-    <div>
-        <label for="">  </label>
-        <input>
-    </div>
+        <div>
+            <label for="">  </label>
+            <input>
+        </div>
 
-    <div>
-        <label for="">  </label>
-        <input>
-    </div>
+        <div>
+            <label for="">  </label>
+            <input>
+        </div>
 
-    <div>
-        <label for="">  </label>
-        <input>
-    </div>
+        <div>
+            <label for="">  </label>
+            <input>
+        </div>
 
-    <div>
-        <label for="">  </label>
-        <input>
-    </div>
+        <div>
+            <label for="">  </label>
+            <input>
+        </div>
 
-    <div>
-        <label for="">  </label>
-        <input>
-    </div>
+        <div>
+            <label for="">  </label>
+            <input>
+        </div>        
+    </form>
+
 </main>
+
+<?php
+    include_once ("footer.php");
+?>
