b88c9be53841094f8bdfa3bab1947620e75373dc
echeverriaalex <aleex.naahuel@outlook.com>
2020-10-28 17:09:24 -0300

    actualizacion de getters y setters de users

--- Models/User.php
+++ Models/User.php
@@ -13,7 +13,7 @@
 
         function __construct ($email='', $password='', $id='', $name='', $lastName='', $gener='', $dni=''){
 
-            $this->setUser($email);
+            $this->setEmail($email);
             $this->setPassword($password);
             $this->setId($id);
             $this->setName($name);
@@ -22,25 +22,25 @@
             $this->setDni($dni);
         }
 
-        public function setUser($email){$this->email = $email;}
-        public function getUser(){return $this->email;}
+        public function setEmail($email){$this->email = $email;}
+        public function getEmail(){return $this->email;}
 
         public function setPassword($password){$this->password = $password;}
         public function getPassword(){return $this->password;}
 
         public function setId($id){$this->id = $id;}
-        public function getId(){return $this->id;}
-
-        public function setLastName($a){ $this->lastName=$a;}
-        public function getLastName(){return $this->lastName;}
+        public function getId(){return $this->id;}       
 
         public function setName($a){$this->name=$a;}
         public function getName(){return $this->name;}
+        
+        public function setLastName($a){ $this->lastName=$a;}
+        public function getLastName(){return $this->lastName;}
 
         public function setGender($gender){$this->gender = $gender;}
         public function getGender(){return $this->gender;}
         
         public function setDni($dni){$this->dni=$dni;}
         public function getDni(){return $this->dni;}        
-}
+    }
 ?>
