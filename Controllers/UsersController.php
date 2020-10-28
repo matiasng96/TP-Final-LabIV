1e203f7087d0bdc8920adaacf62d4ae9144b89d0
nicolasestigarribia <nicolasestigarribia5@gmail.com>
2020-10-28 20:14:21 -0300

    PDO USERS

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -1,27 +1,57 @@
 <?php
     namespace Controllers;
 
-    class UsersController{
+    use Model\User as User;
+    use DAO\UserPDO as UserDao;
+
+   class UsersController{
 
         public function ShowAddView(){
 
             require_once(VIEWS_PATH."registry.php");
         }
 
-        public function logIn($email, $password){
+        public function setSession($user)
+        {
+            $_SESSION["userLogedIn"] = $user;
+        }
 
-            echo $email;
-            echo $password;
+        public function logIn($email, $password)
+        {
+            $D_user = new UserDao();
 
+            $user= $D_user->read($email);
+            //FUNCION READ DEL DAOUSER (EMAIL) 
 
+            if($user)
+            {
+                if($user->getPassword() == $password)
+                {
+                    $this->setSession($user);
+                    return $user;
+                }
+            }
+            return false;
+        }
 
+        public function Add ($user)
+        {
+            $D_user= new UserDao();
+
+            try {
+                $D_user->Add($user);
+                return true;
+
+            } catch (Throwable $ex) {
+                throw $ex;
+            }
         }
 
 
         public function SingUp($name, $lastName, $gender, $dni, $email, $password){
 
             echo "$name, $lastName, $gender, $dni, $email, $password";
-            require_once(VIEWS_PATH."login.php");
+            //require_once(VIEWS_PATH."login.php");
 
         }
     }
