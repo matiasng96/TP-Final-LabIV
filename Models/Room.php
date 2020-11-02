43b4b5cdb7a471eaed0a87417bd2f3957aca2d15
Matías Gallego <matiasngallego@gmail.com>
2020-11-02 12:58:18 -0300

    Refactor de muchas cosas

--- Models/Room.php
+++ Models/Room.php
@@ -9,10 +9,34 @@
         private $capacity;
 
 
+        public function getName()
+        {
+                return $this->name;
+        }
+
+        public function setName($name)
+        {
+                $this->name = $name;
+        }
+
+
+        public function getTicketPrice()
+        {
+                return $this->ticketPrice;
+        }
+
+
+        public function setTicketPrice($ticketPrice)
+        {
+                $this->ticketPrice = $ticketPrice;
+        }
+
+   
         public function getCapacity()
         {
                 return $this->capacity;
         }
+
 
         public function setCapacity($capacity)
         {
--- Moviepass.sql
+++ Moviepass.sql
@@ -1,26 +1,31 @@
 CREATE DATABASE Moviepass;
 USE Moviepass;
 
-SELECT * FROM genresxmovies;
-
 
 CREATE TABLE IF NOT EXISTS cinemas(
 Id_cinema INT NOT NULL AUTO_INCREMENT,
-C_name VARCHAR(30) NOT NULL,
-Capacity SMALLINT NOT NULL,
-C_address VARCHAR(20) NOT NULL,
-TicketPrice SMALLINT NOT NULL,
+CinemaName VARCHAR(30) NOT NULL,
+TotalCapacity SMALLINT NOT NULL,
+CinemaAddress VARCHAR(20) NOT NULL,
 CONSTRAINT `PK-Id_cinema` PRIMARY KEY (Id_cinema),
-CONSTRAINT unq_C_name UNIQUE (C_name),
-CONSTRAINT unq_C_address UNIQUE (C_address)
+CONSTRAINT unq_C_name UNIQUE (CinemaName),
+CONSTRAINT unq_C_address UNIQUE (CinemaAddress)
 );
+
+CREATE TABLE IF NOT EXISTS rooms(
+Id_room INT NOT NULL AUTO_INCREMENT,
+RoomName VARCHAR(30) NOT NULL,
+Capacity SMALLINT NOT NULL,
+CONSTRAINT `PK-Id_room` PRIMARY KEY (Id_room),
+);
+
 
 DROP DATABASE Moviepass;
 DROP TABLE genres;
 CREATE TABLE IF NOT EXISTS movies(
 Id_movie INT NOT NULL,
 Id_genre INT NOT NULL,
-M_name VARCHAR(30) NOT NULL,    
+MovieName VARCHAR(30) NOT NULL,    
 Poster_path VARCHAR(200)  NOT NULL,
 Title VARCHAR(30) NOT NULL,
 CONSTRAINT `PK-Id_movie` PRIMARY KEY (Id_movie),
@@ -29,21 +34,19 @@ CONSTRAINT `FK-Id_genre` FOREIGN KEY (Id_genre) REFERENCES genres (Id_genre)
 
 CREATE TABLE IF NOT EXISTS genres(
 Id_genre INT NOT NULL,
-G_name VARCHAR(30) NOT NULL,
+GenreName VARCHAR(30) NOT NULL,
 CONSTRAINT `PK-Id_genre` PRIMARY KEY (Id_genre)
 );
 
-CREATE TABLE IF NOT EXISTS genresXmovies(
-Id_genresXmovies INT NOT NULL AUTO_INCREMENT,
-Id_movie INT NOT NULL,
-Id_genre INT NOT NULL,
-CONSTRAINT `PK-Id_genresXmovies` PRIMARY KEY (Id_genresXmovies),
-CONSTRAINT `FK-Id_movie` FOREIGN KEY (Id_movie) REFERENCES movies (Id_movie),
-CONSTRAINT `FK-Id_genre` FOREIGN KEY (Id_genre) REFERENCES genres (Id_genre)
-);
+-- CREATE TABLE IF NOT EXISTS genresXmovies(
+-- Id_genresXmovies INT NOT NULL AUTO_INCREMENT,
+-- Id_movie INT NOT NULL,
+-- Id_genre INT NOT NULL,
+-- CONSTRAINT `PK-Id_genresXmovies` PRIMARY KEY (Id_genresXmovies),
+-- CONSTRAINT `FK-Id_movie` FOREIGN KEY (Id_movie) REFERENCES movies (Id_movie),
+-- CONSTRAINT `FK-Id_genre` FOREIGN KEY (Id_genre) REFERENCES genres (Id_genre)
+-- );
 
-/*INSERT INTO genres(Id_genre, G_name) VALUES (28,"Action"), (12,"Adventure" ), (, ), (, ), (, ),
-(, ),*/
 
 CREATE TABLE IF NOT EXISTS users(
 UserId INT NOT NULL AUTO_INCREMENT,
@@ -55,7 +58,3 @@ UserGender VARCHAR(40) NOT NULL,
 UserDni BIGINT NOT NULL,
 CONSTRAINT `PK-Id_users` PRIMARY KEY (UserId)
 );
-
-
-
-DESCRIBE users;
