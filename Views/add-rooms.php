dec63bf65a3585b70a214b41f39af278f0a0e44d
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-03 11:18:27 -0300

    modificamos los formularios

---
+++ Views/add-rooms.php
@@ -0,0 +1,22 @@
+<div>
+    <form action="<?php echo FRONT_ROOT?> " method="POST">
+        <div>
+            <label>Nombre de la sala</label>
+            <input >
+        </div>
+
+        <div>
+            <label>Precio de la entrada</label>
+            <input>
+        </div>
+        
+        <div>
+            <label>Capacidad</label>
+            <input type="number">
+        </div>
+
+        <div>
+            <button type="submit"> Agregar </button>
+        </div>
+    </form>
+</div>
