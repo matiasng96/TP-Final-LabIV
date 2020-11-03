b63cf2ccc43723c6e1de36743c0720802761688c
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-03 12:05:08 -0300

    modificacion moviepdo

--- DAO/MoviesPDO.php
+++ DAO/MoviesPDO.php
@@ -4,6 +4,7 @@
 
     use DAO\IMoviesDAO as IMoviesDAO;
     use Models\Movie as Movie;
+    use \Exception as Exception;
 
     class MoviesPDO implements IMoviesDAO
     {
@@ -11,13 +12,51 @@
 
         public function Add(Movie $movie)
         {
+            try{
+            $sql= "INSERT INTO ". $this->tableName. "(Id_movie, Poster_path, Title) 
+                                                    VALUE (:id, :poster_path, :title);";
+                                                    
+            $parameters["id"]= $movie->getId(); 
+            $parameter["genero"]= 
+            $parameters["poster_path"]= $movie->getPoster_path();
+            $parameters["title"]= $movie->getTitle();
 
-          
+            $this->connection = Connection :: GetInstance();
+
+            $this->connection->ExecuteNonQuery($sql , $parameters);
+            
+            }catch(Exception $ex){
+                throw $ex;
+            }
         }
 
 
         public function GetAll()
         {
+            try{
+            $sql= "SELECT * FROM ".$this->tableName.";";
+                        
+                $this->connection= Connection::getInstance();
+                $result = $this->connection->Execute($sql);
+               
+                
+                foreach ($result as $value)
+                {
+                    $movie = new Movie();
+                    $movie->setId($value["Id_movie"]);
+                    $movie->setPoster_path($value["Poster_path"]);
+                    $movie->setTitle($value["Title"]);
+
+                    array_push($this->moviesList, $movie);
+
+                }
+
+                return $this->moviesList;
+
+            }catch(Exception $ex){
+                                
+                throw $ex;
+            }
 
         }
 
@@ -44,7 +83,7 @@
                 $movie = new Movie();
                 $movie->setPoster_path($data['poster_path']);
                 $movie->setId($data['id']);
-                $movie->setGenre_ids($data['genre_ids']);
+                //$movie->setGenre_ids($data['genre_ids']);
                 $movie->setTitle($data['title']);
             
                 array_push($moviesArray, $movie);
