8b20d06cf59d733456bf7702396fae33243682e5
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-02 23:22:23 -0300

    retoque visual

--- DAO/RoomPDO.php
+++ DAO/RoomPDO.php
@@ -1,33 +1,30 @@
 <?php
+    namespace DAO;
+    use Models\Room as Room;
+    use \Exception as Exception;
 
-namespace DAO;
-use Models\Room;
-use \Exception as Exception;
+    class RoomPDO {
 
-class RoomPDO {
-
-    private $connection;
-    private $tableName = "rooms";
+        private $connection;
+        private $tableName = "rooms";
 
 
-public function Add(Room $room)
-{
-    try {
-        $query = "INSERT INTO " . $this->tableName . " (R_name, TicketPrice, Capacity) VALUES (:R_name, :TicketPrice, :Capacity);";
+        public function Add(Room $room)
+        {
+            try {
+                $query = "INSERT INTO " . $this->tableName . " (R_name, TicketPrice, Capacity) VALUES (:R_name, :TicketPrice, :Capacity);";
 
-        $parameters["R_name"] = $room->getName();
-        $parameters["TicketPrice"] = $room->getTicketPrice();
-        $parameters["Capacity"] = $room->getCapacity();
-        
-        $this->connection = Connection::GetInstance();
+                $parameters["R_name"] = $room->getName();
+                $parameters["TicketPrice"] = $room->getTicketPrice();
+                $parameters["Capacity"] = $room->getCapacity();
+                
+                $this->connection = Connection::GetInstance();
 
-        $this->connection->ExecuteNonQuery($query, $parameters);
+                $this->connection->ExecuteNonQuery($query, $parameters);
 
-    } catch (Exception $ex) {
-        throw $ex;
+            } catch (Exception $ex) {
+                throw $ex;
+            }
+        }
     }
-}
-
-}
-
 ?>
