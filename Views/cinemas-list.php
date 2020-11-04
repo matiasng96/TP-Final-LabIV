fcff3d6e038133c6e1b3edcb5287165d9d195e21
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-04 20:15:03 -0300

    agrego funcionalidades al listar los cines

--- Views/cinemas-list.php
+++ Views/cinemas-list.php
@@ -8,6 +8,7 @@ require_once('nav.php');
             <th scope="col">Nombre</th>
             <th scope="col">Capacidad</th>
             <th scope="col">Dirección</th>
+            <th scope="col">Salas</th>
             <th scope="col">Acciones</th>
         </thead>
         <tbody>
@@ -15,28 +16,38 @@ require_once('nav.php');
             foreach ($cinemasList as $cinema) {
             ?>
                 <tr>
-                    <td>
-                        <?php echo $cinema->getName() ?>
-                    </td>
+                    <td><?php echo $cinema->getName(); ?></td>
+                    <td><?php echo $cinema->getTotalCapacity(); ?></td>
+                    <td><?php echo $cinema->getAddress(); ?></td>
 
                     <td>
-                        <?php echo $cinema->getTotalCapacity() ?>
+                        <?php 
+                            if($cinema->getRooms()){
+
+                                foreach($cinema->getRooms() as $room){
+                                    
+                                    require_once(VIEWS_PATH."rooms-list.php");
+                                }                            
+                            }
+                        ?>
                     </td>
 
-                    <td>
-                        <?php echo $cinema->getAddress() ?>
-                    </td>
                     <td>
                         <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/Delete">
-                        <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
-                        <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn" >Borrar</button>
+                            <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
+                            <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn"> Borrar </button>
                         </form>
 
                         <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/ShowEditview">
-                        <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
-                        <input type="hidden" name="cinemaCapacity" value="<?php echo $cinema->getTotalCapacity() ?>">
-                        <input type="hidden" name="cinemaAddress" value="<?php echo $cinema->getAddress() ?>">
-                        <button class="btn btn-primary btn-block" type="submit" name="editBtn" >Editar</button>
+                            <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
+                            <input type="hidden" name="cinemaCapacity" value="<?php echo $cinema->getTotalCapacity() ?>">
+                            <input type="hidden" name="cinemaAddress" value="<?php echo $cinema->getAddress() ?>">
+                            <button class="btn btn-primary btn-block" type="submit" name="editBtn"> Editar </button>
+                        </form>
+
+                        <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowAddView">
+                            <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
+                            <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn"> Agregar Salas </button>
                         </form>
                     </td>
 
