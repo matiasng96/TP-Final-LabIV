4acf2924ad986c14e5fa0d7cef218e20a203ca89
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-04 20:17:31 -0300

    completo los metodos y corrijo parametros

--- Controllers/RoomsController.php
+++ Controllers/RoomsController.php
@@ -1,27 +1,31 @@
 <?php
     namespace Controllers;
-    use DAO\RoomsPDO;
+    use DAO\RoomsPDO as RoomsPDO;
+    use Models\Room as Room;
 
     class RoomsController{
 
         private $rooomPDO;
 
-        public function __construct(){$this->rooomPDO = new RoomsPDO();}
+        public function __construct(){$this->rooomPDO = new RoomsPDO;}
 
         public function ShowListView(){
 
+            $roomList = $this->rooomPDO->getAll();
+            require_once(VIEWS_PATH."rooms-list.php");
         }
 
-        public function ShowAddView(){
+        public function ShowAddView(){require_once(VIEWS_PATH."add-rooms.php");}
 
+        public function Add($name, $price, $capacity){
+
+            $room = new Room();
+            $room->setName($name);
+            $room->setTicketPrice($price);
+            $room->setCapacity($capacity);
+            
+            $this->rooomPDO->Add($room);
+            $this->ShowAddView();
         }
-
-        
-
-
-
-
-
     }
-
 ?>
