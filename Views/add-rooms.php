a240eb7e54d7bbf5fd2cd43fa1cc1ff11dd2df12
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-04 11:10:21 -0300

    view de agregar salas finalizada

--- Views/add-rooms.php
+++ Views/add-rooms.php
@@ -2,27 +2,31 @@
     require_once("nav.php");
 ?>
 
-<div>
-    <h1> Agregar una sala nueva</h1>
+<div class="container row justify-content-center">
+    <div class="form-group mt-4 border borderForm">   
+        <form action="<?php echo FRONT_ROOT?>Room/Add" method="POST">        
+            <div class="col-auto">
+                <h2> Agregar una sala nueva</h2>
+            </div>
 
-    <form action="<?php echo FRONT_ROOT?>Room/Add" method="POST">
-        <div>
-            <label>Nombre de la sala</label>
-            <input >
-        </div>
+            <div class="col-auto">
+                <label for="name">Nombre de la sala</label>
+                <input class="form-control" type="text" name="name" id="name" required>
+            </div>
 
-        <div>
-            <label>Precio de la entrada</label>
-            <input>
-        </div>
-        
-        <div>
-            <label>Capacidad</label>
-            <input type="number">
-        </div>
+            <div class="col-auto">
+                <label for="price">Precio de la entrada</label>
+                <input class="form-control" type="number" name="price" min=0 id="price" required>
+            </div>
+            
+            <div class="col-auto">
+                <label for="capacity">Capacidad</label>
+                <input class="form-control" type="number" name="capacity" min=0 id="capacity" required>
+            </div>
 
-        <div>
-            <button type="submit"> Agregar </button>
-        </div>
-    </form>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mt-2 mb-2"> Agregar </button>
+            </div>
+        </form>
+    </div>   
 </div>
