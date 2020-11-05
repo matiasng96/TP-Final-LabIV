3501c6741cc7095952852bc3fafb7276bfb914fb
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-05 11:51:14 -0300

    modificaciones visuales

--- Views/cinemas-list.php
+++ Views/cinemas-list.php
@@ -42,14 +42,14 @@ require_once('nav.php');
                                             <td>
                                                 <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/Delete">
                                                     <input type="hidden" name="roomName" value="<?php echo $room->getName() ?>">
-                                                    <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn" >Borrar</button>
+                                                    <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn" >Borrar sala</button>
                                                 </form>
 
                                                 <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowEditview">
                                                     <input type="hidden" name="roomName" value="<?php echo $room->getName(); ?>">
                                                     <input type="hidden" name="roomCapacity" value="<?php echo $room->getCapacity(); ?>">
                                                     <input type="hidden" name="roomPrice" value="<?php echo $room->getTicketPrice(); ?>">
-                                                    <button class="btn btn-primary btn-block" type="submit" name="editBtn" >Editar</button>
+                                                    <button class="btn btn-primary btn-block" type="submit" name="editBtn" >Editar sala</button>
                                                 </form>
                                             </td>
                                         </tr>
@@ -63,14 +63,14 @@ require_once('nav.php');
                     <td>
                         <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/Delete">
                             <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
-                            <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn"> Borrar </button>
+                            <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn"> Borrar cine</button>
                         </form>
 
                         <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/ShowEditview">
                             <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
                             <input type="hidden" name="cinemaCapacity" value="<?php echo $cinema->getTotalCapacity() ?>">
                             <input type="hidden" name="cinemaAddress" value="<?php echo $cinema->getAddress() ?>">
-                            <button class="btn btn-primary btn-block" type="submit" name="editBtn"> Editar </button>
+                            <button class="btn btn-primary btn-block" type="submit" name="editBtn"> Editar cine </button>
                         </form>
 
                         <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowAddView">
