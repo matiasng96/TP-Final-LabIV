85b5586ac5c20135a3478e14843bc6a49e81b7ae
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-05 11:49:02 -0300

    incorporo el listado de salas en cada cine

--- Views/cinemas-list.php
+++ Views/cinemas-list.php
@@ -22,14 +22,42 @@ require_once('nav.php');
 
                     <td>
                         <?php 
-                            if($cinema->getRooms()){
+                            if($cinema->getRooms()){ 
+                        ?>                                
+                               <table class="table table-bordered ">
+                                    <thead>
+                                        <th scope="col"> Nombre </th>            
+                                        <th scope="col"> Capacidad </th>
+                                        <th scope="col"> Precio de la Sala </th>
+                                    </thead>
 
-                                foreach($cinema->getRooms() as $room){
-                                    
-                                    require_once(VIEWS_PATH."rooms-list.php");
-                                }                            
-                            }
-                        ?>
+                                    <?php
+                                        foreach($cinema->getRooms() as $room){
+                                    ?>  
+                                        <tr>
+                                            <td> <?php echo $room->getName();        ?> </td>
+                                            <td> <?php echo $room->getCapacity();    ?> </td>
+                                            <td> <?php echo $room->getTicketPrice(); ?> </td>
+
+                                            <td>
+                                                <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/Delete">
+                                                    <input type="hidden" name="roomName" value="<?php echo $room->getName() ?>">
+                                                    <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn" >Borrar</button>
+                                                </form>
+
+                                                <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowEditview">
+                                                    <input type="hidden" name="roomName" value="<?php echo $room->getName(); ?>">
+                                                    <input type="hidden" name="roomCapacity" value="<?php echo $room->getCapacity(); ?>">
+                                                    <input type="hidden" name="roomPrice" value="<?php echo $room->getTicketPrice(); ?>">
+                                                    <button class="btn btn-primary btn-block" type="submit" name="editBtn" >Editar</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                        }
+                                    ?>
+                                </table>
+                        <?php }else{echo "Este cine no contiene salas.";}?>
                     </td>
 
                     <td>
