aa780c4afcd5b14d23c30cfd1a02f1aa3fc1db35
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-05 17:27:09 -0300

    cambio las vista de inico

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -5,10 +5,10 @@
     {
         public function Index($message = "")
         {
-            //require_once(VIEWS_PATH."add-cinema.php");
+            require_once(VIEWS_PATH."add-cinema.php");
            //require_once(VIEWS_PATH."buyTickets.php");
            
-           require_once(VIEWS_PATH."cinemas-list.php");
+           //require_once(VIEWS_PATH."cinemas-list.php");
            //require_once(VIEWS_PATH."rooms-list.php");
         }        
     }
