6457770344d94a871eb6e1e4f34e3c989fc0e93a
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-10 17:26:45 -0300

    arreglos de variables

--- DAO/CinemasPDO.php
+++ DAO/CinemasPDO.php
@@ -5,6 +5,7 @@
     use \PDOException as Exception;
     use DAO\Connection as Connection;
     use Models\Cinema as Cinema;
+    use Models\Room as Room;
 
     class CinemasPDO implements ICinemasDAO
     {
@@ -179,7 +180,7 @@
                 $cinemasResults = $this->connection->Execute($query);
 
                 //Recorro el arreglo de Cines(dela base de datos) y tomo los datos de cada fila para crear un objeto Cinema y guardo cada objeto en $cinemasList.
-                $room= new RoomPDO();
+                $room= new RoomsPDO();
                 foreach ($cinemasResults as $row) {
                     $cinema = new Cinema();
                     $cinema->setName($row['CinemaName']);
