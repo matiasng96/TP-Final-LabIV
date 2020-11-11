aba9ea1f42dd39e341f30285291ba88d17def296
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-11 17:17:19 -0300

    test de como se veria el perfil

---
+++ Views/infoUser.php
@@ -0,0 +1,26 @@
+<?php
+    require_once("nav.php");
+?>
+<div>
+    <div>
+        <label > Nombre </label>
+        
+    </div>
+
+    <div>
+        
+    </div>
+    
+    <div>
+        
+    </div>
+    
+    <div>
+        
+    </div>
+
+    <div>
+        
+    </div>
+
+</div>
