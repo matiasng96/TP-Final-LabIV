782788c1b50d4613006d3cc6d57ef20d7fee6767
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-11 17:16:18 -0300

    optimizacion

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -6,7 +6,7 @@
     use Models\Rol as Rol;
     use DAO\UsersPDO as UserDAO;
     use DAO\UserRolePDO as RolePDO;
-    
+    use Exception as Exception;
 
    class UsersController{
        
@@ -44,12 +44,7 @@
         public function logIn($email, $password)
         {
             $user= $this->userDAO->read($email);   
-            //echo('<pre>');
-            //var_dump($user);
-            //echo('</pre>');    
-            
-            
-            
+         
             if(strcmp($user->getEmail(), $email) == 0)
             {
                 if(strcmp($user->getPassword(), $password)== 0)
@@ -81,61 +76,56 @@
             require_once(VIEWS_PATH . "login.php");
         }
     
-        public function Edit($name, $lastName, $gender, $dni, $email, $password ){
+        public function Edit($currentEmail, $name, $lastName, $gender, $dni, $email, $password ){
 
-            $user = $this->userDAO->read($email);
-            $user = new User($user->getId(), $email, $password, $name, $lastName, $gender, $dni);
-            $this->userDAO->Add($user);
+            try{
+                $user = new User($name, $lastName, $gender, $dni, $email, $password);
+                $userEdited = $this->userDAO->Edit($currentEmail, $user);
+                var_dump($userEdited);
+            }
+            catch(Exception $ex){
 
-
-        }
-
-        public function setUser ($name, $lastName, $gender, $dni, $email, $password)
-        {
-            $aux= new User();
-            $aux->setEmail($email);
-            $aux->setPassword($password);
-            $aux->setName($name);
-            $aux->setLastName($lastName);
-            $aux->setGender($gender);
-            $aux->setDni($dni);
-
-            return $aux;
-        }
+                echo "<script> Se produjo un error al querer modificar la informacion. </script>";
+            }
+            //$user = $this->userDAO->read($email);
+            //$this->userDAO->Add($user);
+        }      
 
         public function SignUp($name, $lastName, $gender, $dni, $email, $password)
         {
-
-            $exist= $this->userDAO->read($email);
-           
+            $exist= $this->userDAO->read($email);           
             
             ///PREGUNTA SI ESTA SETTEADO O ES DISTINTA DE NULL LA VARIABLE EXIST
-            if($exist->getEmail() == NULL)
-            {
+            if($exist->getEmail() == NULL){
              
-            $roleArray= $this->userRole->retrieveAll();//TRAIGO EL ARREGLO DE ROLES, NECESARIO PARA AGREGAR ROL AL USUARIO
-            $flag= $this->userDAO->checkUserList(); ///RETORNA EL LA CANTIDAD DE USUARIOS (quantity)
+                $roleArray= $this->userRole->retrieveAll();//TRAIGO EL ARREGLO DE ROLES, NECESARIO PARA AGREGAR ROL AL USUARIO
+                $flag= $this->userDAO->checkUserList(); ///RETORNA EL LA CANTIDAD DE USUARIOS (quantity)
 
-            $user = new User();
-             
-            $user= $this->setUser($name, $lastName, $gender, $dni, $email, $password);
+                
 
-            if($flag==0)
-            {
-                $user->setUserRole($roleArray[0]);
-            }else
-            {
-                $user->setUserRole($roleArray[1]);
-            }
-            $this->userDAO->Add($user);
+                $user = new User($name, $lastName, $gender, $dni, $email, $password);
+                $rol = new Rol();
+                
+                var_dump($roleArray);
 
-            require_once(VIEWS_PATH."login.php");  // VISTA DE LOGGEO
-           }else
-            {
-            
-            require_once(VIEWS_PATH."registry.php"); /// VISTA DE REGISTRO
-            }
-         
+                if($flag==0){
+
+                    $rol->setRol($roleArray[0]);
+                    $user->setUserRole($rol);
+                }
+                else{
+                    $rol->setRol($roleArray[1]);
+                    $user->setUserRole($rol);
+                }
+
+                $this->userDAO->Add($user);
+
+                require_once(VIEWS_PATH."login.php");  // VISTA DE LOGGEO
+           }
+           else{
+
+                require_once(VIEWS_PATH."registry.php"); /// VISTA DE REGISTRO
+           }    
         }           
     }        
 ?>
