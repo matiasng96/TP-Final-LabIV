552d6171a4506690da290e98e3f28cd16a92ee35
nicolasestigarribia <nicolasestigarribia5@gmail.com>
2020-11-13 11:43:37 -0300

    Funcionando por ahora.

--- Data/Moviepass.sql
+++ Data/Moviepass.sql
@@ -1,26 +1,23 @@
 CREATE DATABASE Moviepass;
 USE Moviepass;
-#DROP DATABASE moviepass;
-
+DROP DATABASE moviepass;
 SELECT * FROM movies;
 SELECT * FROM genres;
 SELECT * FROM genresXmovies;
+DROP TABLE genresXmovies;
 
-SELECT r.Id_room,r.RoomName,r.Capacity,r.TicketPrice,c.CinemaName 
-			FROM rooms r INNER JOIN cinemas c ON c.Id_cinema=r.Id_cinema;
-#===============TABLA CINEMAS=============================
 CREATE TABLE IF NOT EXISTS cinemas(
 Id_cinema INT NOT NULL AUTO_INCREMENT,
 CinemaName VARCHAR(30) NOT NULL,
 TotalCapacity SMALLINT NOT NULL,
 CinemaAddress VARCHAR(20) NOT NULL,
 CONSTRAINT pk_IdCinema PRIMARY KEY (Id_cinema),
-CONSTRAINT unq_Cinema_name UNIQUE (CinemaName),
-CONSTRAINT unq_Cinema_address UNIQUE (CinemaAddress)
+CONSTRAINT unq_CinemaName UNIQUE (CinemaName),
+CONSTRAINT unq_CinemaAddress UNIQUE (CinemaAddress)
 );
+SELECT r.Id_room,r.RoomName,r.Capacity,r.TicketPrice,c.CinemaName 
+			FROM rooms r INNER JOIN cinemas c ON c.Id_cinema=r.Id_cinema;
 
-
-#===============TABLA SALAS=============================
 CREATE TABLE IF NOT EXISTS rooms(
 Id_room INT NOT NULL AUTO_INCREMENT,
 Id_cinema INT NOT NULL,
@@ -32,14 +29,17 @@ CONSTRAINT pk_IdRoom PRIMARY KEY(Id_room),
 CONSTRAINT fk_idCinema FOREIGN KEY (Id_cinema) REFERENCES cinemas (Id_cinema)
 );
 
-#===============TABLA PELICULAS=============================
+select * from cinemas inner join rooms on cinemas.Id_cinemas = rooms.Id_cinema;
+select * from rooms inner join cinemas  on cinemas.Id_cinema = rooms.Id_cinema;
+
 CREATE TABLE IF NOT EXISTS movies(
-Id_movie INT NOT NULL,  
-Poster_path VARCHAR(200)  NOT NULL,
-Runtime SMALLINT NOT NULL,
-Original_language VARCHAR(10),
-Title VARCHAR(30) NOT NULL,
-CONSTRAINT `PK-Id_movie` PRIMARY KEY (Id_movie)
+Id_movie INT NOT NULL,
+Id_genre INT NOT NULL,
+MovieName VARCHAR(30) NOT NULL,    
+Id_room INT NOT NULL AUTO_INCREMENT,
+RoomName VARCHAR(30) NOT NULL,
+Capacity SMALLINT NOT NULL,
+CONSTRAINT pk_IdRoom PRIMARY KEY (Id_room)
 );
 
 
@@ -55,21 +55,29 @@ WHERE(m.Id_movie = '425001');
 */
 INSERT INTO genresXmovies (Id_movie,Id_genre) VALUES(1,3);
 
-#===============TABLA GENEROS=============================
+CREATE TABLE IF NOT EXISTS movies(
+Id_movie INT NOT NULL,  
+Poster_path VARCHAR(200)  NOT NULL,
+Runtime SMALLINT NOT NULL,
+Original_language VARCHAR(10),
+Title VARCHAR(30) NOT NULL,
+CONSTRAINT pk_IdMovie PRIMARY KEY (Id_movie),
+CONSTRAINT fk_IdGenre FOREIGN KEY (Id_genre) REFERENCES genres (Id_genre)
+);
+
 CREATE TABLE IF NOT EXISTS genres(
 Id_genre INT NOT NULL,
 GenreName VARCHAR(30) NOT NULL,
-CONSTRAINT `PK-Id_genre` PRIMARY KEY (Id_genre)
+CONSTRAINT pk_IdGenre PRIMARY KEY (Id_genre)
 );
 
-#===============TABLA GENEROSXPELICULAS=============================
 CREATE TABLE IF NOT EXISTS genresXmovies(
 Id_genresXmovies INT NOT NULL AUTO_INCREMENT,
 Id_movie INT NOT NULL,
 Id_genre INT NOT NULL,
-CONSTRAINT `PK-Id_genresXmovies` PRIMARY KEY (Id_genresXmovies),
-CONSTRAINT `FK-Id_movie` FOREIGN KEY (Id_movie) REFERENCES movies (Id_movie),
-CONSTRAINT `FK-Id_genre` FOREIGN KEY (Id_genre) REFERENCES genres (Id_genre)
+CONSTRAINT pk_IdGenresXmovies PRIMARY KEY (Id_genresXmovies),
+CONSTRAINT fk_IdMovie FOREIGN KEY (Id_movie) REFERENCES movies (Id_movie),
+CONSTRAINT fk_IdGenre FOREIGN KEY (Id_genre) REFERENCES genres (Id_genre)
 );
 
 CREATE TABLE IF NOT EXISTS users(
@@ -81,15 +89,15 @@ UserPassword VARCHAR(30) NOT NULL,
 UserGender VARCHAR(40) NOT NULL,
 UserDni BIGINT NOT NULL,
 Id_userRole INT NOT NULL,
-CONSTRAINT `PK-Id_users` PRIMARY KEY (UserId),
-CONSTRAINT `unq_email` UNIQUE(UserEmail),
-CONSTRAINT `fk_users_roles` FOREIGN KEY (Id_userRole) REFERENCES userRole (Id_role)
+CONSTRAINT pk_IdUsers PRIMARY KEY (UserId),
+CONSTRAINT unq_Email UNIQUE(UserEmail),
+CONSTRAINT fk_usersRoles FOREIGN KEY (Id_userRole) REFERENCES userRole (Id_role)
 );
 
 CREATE TABLE IF NOT EXISTS userRole (
 Id_role INT NOT NULL auto_increment,
 RoleDescrip VARCHAR (40) NOT NULL,
-CONSTRAINT `PK-Id_role`PRIMARY KEY (Id_role),
+CONSTRAINT pk_IdRole PRIMARY KEY (Id_role),
 CONSTRAINT unq_user_roles UNIQUE(RoleDescrip)
 );
 
