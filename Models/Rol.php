bfd95e1e4530aff088eaa1ce0064ff0f957fb300
Matias Gallego <matiasngallego@gmail.com>
2020-11-21 18:36:47 -0300

    .

--- Models/Rol.php
+++ Models/Rol.php
@@ -6,10 +6,10 @@
         private $id;
         private $rol;
 
-        function __construct($rol = ''){$this->setRol($rol); }
+        function __construct($rol = ''){$this->setRol($rol);}
       
         public function setRol($rol){$this->rol = $rol;}
-        public function getRol(){return (string)$this->rol;}   
+        public function getRol(){return (string)$this->rol;} 
 
         public function setId($id){$this->id= $id;}             
         public function getId(){return $this->id;}
