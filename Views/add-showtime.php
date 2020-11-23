2a0f1e6d9c2c9e18d647c4769c4641b6fbecac50
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-23 20:55:18 -0300

    agregar showtime

--- Views/add-showtime.php
+++ Views/add-showtime.php
@@ -1,10 +1,60 @@
-<form action="<?php echo FRONT_ROOT ?> Showtime/Add" method="POST" form="formAddShowtime">
-    <div class="col-auto">
-        <h2><?php echo $movie->getTitle() ?></h2>
+<?php
+     use Controllers\NavController;
+     $nav = new NavController();
+     $nav ->selectNav();
+     $rol =  $nav->getRol();
+?>
+<div class="container row justify-content-center">
+    <div class="form-group mt-4 border borderForm">
+        <form action="<?php echo FRONT_ROOT ?> Showtime/Add" method="POST" form="formAddShowtime">
+            <div class="col-auto">
+                <h2><?php //echo $movie->getTitle() ?></h2>
+            </div>
+
+            <div class="col-auto my-1">
+                <label> Seleccionar Cinema </label>
+
+                <?php
+                    foreach($cinemaList as $cinema){
+                ?>
+
+                <select name="cinema">
+                    <option value="<?php $cinema->getName() ?>"> <?php $cinema->getName() ?> </option>
+                </select>
+
+                <?php
+                    }
+                ?>
+            </div>
+
+            <div class="col-auto my-1">
+                <label> Seleccionar Sala </label>
+                <?php
+                    foreach($roomList as $room){
+                ?>
+
+                <select name="room">
+                    <option value="<?php $room->getName() ?>"> <?php $room->getName() ?> </option>
+                </select>
+
+                <?php
+                    }
+                ?>
+            </div>
+
+            <div class="col-auto my-1">
+                <label> Seleccionar Fecha </label>
+                <input name="date" type="date">
+            </div>
+
+            <div class="col-auto my-1">
+                <label> Seleccionar Horario </label>
+                <input name="time" type="time">
+            </div>
+
+            <div class="col-auto my-1">
+                <button class="btn btn-primary" type="submit"> Crear funcion</button>
+            </div>
+        </form>
     </div>
-
-    <div>
-
-
-
-</form>
+</div>
