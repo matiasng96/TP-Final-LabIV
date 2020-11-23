b59acb895bbf66bd7c58e20e08b3daad3b32972b
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-23 20:15:19 -0300

    agrgeo la veriicacion de los roles

--- Views/cinemas-list.php
+++ Views/cinemas-list.php
@@ -12,7 +12,15 @@
             <th scope="col">Capacidad Total</th>
             <th scope="col">Dirección</th>
             <th scope="col">Salas</th>
-            <th scope="col">Opciones</th>
+
+            <?php 
+                if($rol == 1){
+            ?>
+                    <th scope="col">Opciones</th>
+            <?php 
+                }
+            ?>
+              
         </thead>
         <tbody>
             <?php
@@ -32,7 +40,13 @@
                                     <th scope="col"> Nombre </th>
                                     <th scope="col"> Capacidad </th>
                                     <th scope="col"> Precio de la Sala </th>
-                                    <th scope="col"> Opciones </th>
+                                    <?php 
+                                        if($rol == 1){
+                                    ?>
+                                            <th scope="col"> Opciones </th>
+                                            <?php 
+                                        }
+                                    ?>
                                 </thead>
 
                                 <?php
@@ -43,22 +57,28 @@
                                         <td> <?php echo $room->getCapacity();    ?> </td>
                                         <td> <?php echo $room->getTicketPrice(); ?> </td>
 
-                                        <td>
-                                            <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/Delete">
-                                                <input type="hidden" name="roomName" value="<?php echo $room->getName() ?>">
-                                                <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn">Borrar sala</button>
-                                            </form>
+                                        <?php 
+                                            if($rol == 1){
+                                        ?>
+                                            <td>
+                                                <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/Delete">
+                                                    <input type="hidden" name="roomName" value="<?php echo $room->getName() ?>">
+                                                    <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn">Borrar sala</button>
+                                                </form>
 
-                                            <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowEditView">
-                                                <!--<input type="hidden" name="Id_cinema" value="<?php// echo $room->getIdCinema(); ?>">
-                                                    <input type="hidden" name="cinemaName" value="<?php //echo $room->getCinemaName(); 
-                                                                                                    ?>">--->
-                                                <input type="hidden" name="name" value="<?php echo $room->getName(); ?>">
-                                                <input type="hidden" name="capacity" value="<?php echo $room->getCapacity(); ?>">
-                                                <input type="hidden" name="ticketPrice" value="<?php echo $room->getTicketPrice(); ?>">
-                                                <button class="btn btn-primary btn-block" type="submit" name="editBtn">Editar sala</button>
-                                            </form>
-                                        </td>
+                                                <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowEditView">
+                                                    <!--<input type="hidden" name="Id_cinema" value="<?php// echo $room->getIdCinema(); ?>">
+                                                        <input type="hidden" name="cinemaName" value="<?php //echo $room->getCinemaName(); 
+                                                                                                        ?>">--->
+                                                    <input type="hidden" name="name" value="<?php echo $room->getName(); ?>">
+                                                    <input type="hidden" name="capacity" value="<?php echo $room->getCapacity(); ?>">
+                                                    <input type="hidden" name="ticketPrice" value="<?php echo $room->getTicketPrice(); ?>">
+                                                    <button class="btn btn-primary btn-block" type="submit" name="editBtn">Editar sala</button>
+                                                </form>
+                                            </td>
+                                        <?php 
+                                            }
+                                        ?>
                                     </tr>
                                 <?php
                                 }
@@ -71,23 +91,28 @@
                         ?>
                     </td>
 
-                    <td>
-                        <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/Delete">
-                            <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
-                            <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn"> Borrar cine</button>
-                        </form>
+                    <?php 
+                        if($rol == 1){
+                    ?>
+                        <td>
+                            <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/Delete">
+                                <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
+                                <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn"> Borrar cine</button>
+                            </form>
 
-                        <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/ShowEditview">
-                            <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
-                            <input type="hidden" name="cinemaCapacity" value="<?php echo $cinema->getTotalCapacity() ?>">
-                            <input type="hidden" name="cinemaAddress" value="<?php echo $cinema->getAddress() ?>">
-                            <button class="btn btn-primary btn-block" type="submit" name="editBtn"> Editar cine </button>
-                        </form>
+                            <form method="post" action="<?php echo FRONT_ROOT ?>Cinemas/ShowEditview">
+                                <input type="hidden" name="cinemaName" value="<?php echo $cinema->getName() ?>">
+                                <input type="hidden" name="cinemaCapacity" value="<?php echo $cinema->getTotalCapacity() ?>">
+                                <input type="hidden" name="cinemaAddress" value="<?php echo $cinema->getAddress() ?>">
+                                <button class="btn btn-primary btn-block" type="submit" name="editBtn"> Editar cine </button>
+                            </form>
 
-                        <button class="btn btn-danger btn-block mt-2" type="submit" data-toggle="modal" data-target="#addRoom" value="<?php echo $cinema->getName() ?>"> Agregar salas </button>
+                            <button class="btn btn-danger btn-block mt-2" type="submit" data-toggle="modal" data-target="#addRoom" value="<?php echo $cinema->getName() ?>"> Agregar salas </button>
 
-                    </td>
-
+                        </td>
+                    <?php 
+                        }
+                    ?>
                 </tr>
             <?php
             }
--- Views/rooms-list.php
+++ Views/rooms-list.php
@@ -12,7 +12,14 @@
             <th scope="col"> Nombre </th>            
             <th scope="col"> Capacidad </th>
             <th scope="col"> Precio de la Sala </th>
-            <th scope="col"> Opciones </th>
+
+            <?php 
+                if($rol == 1){
+            ?>
+                <th scope="col"> Opciones </th>
+            <?php 
+                }
+            ?>
         </thead>
 
         <tbody>
@@ -24,19 +31,26 @@
                     <td> <?php echo $room->getName();        ?> </td>
                     <td> <?php echo $room->getCapacity();    ?> </td>
                     <td> <?php echo $room->getTicketPrice(); ?> </td>
-                    <td>
-                        <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/Delete">
-                            <input type="hidden" name="roomName" value="<?php echo $room->getName() ?>">
-                            <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn" >Borrar</button>
-                        </form>
 
-                        <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowEditview">
-                            <input type="hidden" name="roomName" value="<?php echo $room->getName(); ?>">
-                            <input type="hidden" name="roomCapacity" value="<?php echo $room->getCapacity(); ?>">
-                            <input type="hidden" name="roomPrice" value="<?php echo $room->getTicketPrice(); ?>">
-                            <button class="btn btn-primary btn-block" type="submit" name="editBtn" >Editar</button>
-                        </form>
-                    </td>
+                    <?php 
+                        if($rol == 1){
+                    ?>
+                            <td>
+                                <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/Delete">
+                                    <input type="hidden" name="roomName" value="<?php echo $room->getName() ?>">
+                                    <button class="btn btn-danger btn-block mb-2" type="submit" name="deleteBtn" >Borrar</button>
+                                </form>
+
+                                <form method="post" action="<?php echo FRONT_ROOT ?>Rooms/ShowEditview">
+                                    <input type="hidden" name="roomName" value="<?php echo $room->getName(); ?>">
+                                    <input type="hidden" name="roomCapacity" value="<?php echo $room->getCapacity(); ?>">
+                                    <input type="hidden" name="roomPrice" value="<?php echo $room->getTicketPrice(); ?>">
+                                    <button class="btn btn-primary btn-block" type="submit" name="editBtn" >Editar</button>
+                                </form>
+                            </td>
+                    <?php 
+                        }
+                    ?>
                 </tr>
             <?php
                 }
