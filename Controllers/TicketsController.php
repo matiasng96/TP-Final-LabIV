e3d7dd18e56a3605404661b1c3b46101c628896c
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-23 17:36:45 -0300

    ticket controller

---
+++ Controllers/TicketsController.php
@@ -0,0 +1,16 @@
+<?php
+
+    class TicketsController{
+
+        private $daoTicket;
+
+        public function Show(){
+
+            require_once("buyTickets.php");
+        }
+
+        public function Add(){
+
+        }
+    }
+?>
