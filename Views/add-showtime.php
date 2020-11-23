83b3d2e80f116cf07a3156739459d8e6e9394aa7
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-23 20:32:22 -0300

    creacion add showtime

---
+++ Views/add-showtime.php
@@ -0,0 +1,10 @@
+<form action="<?php echo FRONT_ROOT ?> Showtime/Add" method="POST" form="formAddShowtime">
+    <div class="col-auto">
+        <h2><?php echo $movie->getTitle() ?></h2>
+    </div>
+
+    <div>
+
+
+
+</form>
