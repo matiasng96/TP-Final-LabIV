aafce8d3a98b46a81267ed7d34121ec635f9cd21
echeverriaalex <aleex.naahuel@outlook.com>
2020-11-23 16:57:12 -0300

    quito el mensaje de alerta

--- Views/navSelector.php
+++ Views/navSelector.php
@@ -3,12 +3,11 @@
     if(isset($_SESSION['userLogedIn'])){
     $rol = $_SESSION['userLogedIn']->getUserRoleId();
     switch($rol){
-         case 1: require_once(VIEWS_PATH."nav.php"); break;
+         case 1: require_once(VIEWS_PATH."nav-admin.php"); break;
          case 2: require_once(VIEWS_PATH."nav-logged.php"); break;
     }  
 }      
-else{           
-    echo "<script> alert('Estas navegando sin haber iniciado sesion.'); </script>";
+else{
     require_once(VIEWS_PATH."nav-notLogged.php");
 }      
 ?>
